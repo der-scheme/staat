3a1ef466d79d962b823358ae21ed69ab5b7c3775
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-09-19 19:53:31 +0200

    Add event manager spec stub.

--- spec/staat/event/manager_spec.rb
+++ spec/staat/event/manager_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Staat::Event::Manager do
+  describe '#<<' do
+    # Simple store & lookup stuff
+  end
 
+  describe '#[]' do
+    # Complex selector stuff
+  end
+
+  describe '#clear' do
+    it 'should remove all Anticipations' do
+      # test here
+    end
+  end
 end

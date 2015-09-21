2e442168c4de38b838d5758aa54bc954eef4f57f
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-09-21 21:31:15 +0200

    Add actual, currently failing, tests.

--- spec/staat/event/manager_spec.rb
+++ spec/staat/event/manager_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
 describe Staat::Event::Manager do
+  let(:a1) {Staat::Event::Anticipation.new(scope: BasicObject, action: :to_s, type: :all, name: :foo) {}}
+  let(:a2) {Staat::Event::Anticipation.new(scope: Array, action: :each, type: :invocatin) {}}
+  let(:a3) {Staat::Event::Anticipation.new(scope: Hash, action: :each, type: :failure, name: :bar) {}}
+  let(:a4) {Staat::Event::Anticipation.new(scope: BasicObject, action: :to_s, type: nil, name: :foo) {}}
+  let(:a5) {Staat::Event::Anticipation.new(scope: String, action: :to_s, type: :completion, name: :baz) {}}
+  let(:empty) {Staat::Event::Manager.new}
+  let(:trivial) do
+    Staat::Event::Manager.new << a1 << a3 << a5
+  end
+  let(:complex) do
+    Staat::Event::Manager.new << a1 << a4 << a5 << a3 << a2
+  end
+
   describe '#<<' do
     it 'should return self' do
+      expect(empty << a1).to be(empty)
+      expect(trivial << a2).to be(trivial)
+      expect(complex << a3).to be(complex)
     end
 
     it 'should store with respect to scope, action, type and name' do
+      expect(trivial[scope: BasicObject, action: :to_s, type: :all, name: :foo]).to contain_exactly(a1)
+      expect(trivial[scope: Hash, action: :each, type: :failure, name: :bar]).to contain_exactly(a3)
+      expect(trivial[scope: String, action: :to_s, type: :completion, name: :baz]).to contain_exactly(a5)
     end
 
     it 'should also store in all superscopes of scope' do
